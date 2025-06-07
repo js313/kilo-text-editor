57d0ba98272c9ca54a0006e932e0b0a18266c8b3
js313 <sharmajeenit2000@gmail.com>
2025-06-07 15:38:07 +0530

    feat: show welcome message

--- kilo.c
+++ kilo.c
@@ -11,6 +11,7 @@
 
 /*** defines ***/
 
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -173,8 +174,28 @@ void editorDrawRows(struct abuf *ab)
 {
     for (int y = 0; y < E.screenrows; y++)
     {
+        if (y == E.screenrows / 3)
+        {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
+
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding)
+            {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--)
+                abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+        {
+            abAppend(ab, "~", 1);
+        }
         abAppend(ab, "\x1b[K", 4);
-        abAppend(ab, "~", 1);
         if (y < E.screenrows - 1)
             abAppend(ab, "\r\n", 3);
     }
