6af0d3a272746909cac44940308b210a5ea40123
js313 <sharmajeenit2000@gmail.com>
2025-07-22 23:47:29 +0530

    feat: highlight single line comments

--- kilo.c
+++ kilo.c
@@ -43,6 +43,7 @@ enum editorKey
 enum editorHighlight
 {
     HL_NORMAL = 0,
+    HL_COMMENT,
     HL_STRING,
     HL_NUMBER,
     HL_MATCH
@@ -57,6 +58,7 @@ struct editorSyntax
 {
     char *filetype;
     char **filematch;
+    char *singleline_comment_start;
     int flags;
 };
 
@@ -96,6 +98,7 @@ char *C_HL_Extensions[] = {".c", ".h", ".cpp", NULL};
 struct editorSyntax HLDB[] = {
     {"c",
      C_HL_Extensions,
+     "//",
      HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
 };
 
@@ -292,6 +295,9 @@ void editorUpdateSyntax(erow *row)
     if (E.syntax == NULL)
         return;
 
+    char *scs = E.syntax->singleline_comment_start;
+    int scs_len = scs ? strlen(scs) : 0;
+
     int prev_sep = 1;
     int in_string = 0;
 
@@ -300,6 +306,15 @@ void editorUpdateSyntax(erow *row)
     {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if (scs_len && !in_string)
+        {
+            if (!strncmp(&row->render[i], scs, scs_len))
+            {
+                memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+                break;
+            }
+        }
 
         if (E.syntax->flags & HL_HIGHLIGHT_STRINGS)
         {
@@ -349,6 +364,8 @@ int editorSyntaxToColor(int hl)
 {
     switch (hl)
     {
+    case HL_COMMENT:
+        return 36;
     case HL_STRING:
         return 35;
     case HL_NUMBER:
